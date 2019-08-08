7e0ef1773dc285fdb49717932ce59012394280e9
TannorWallace <tannor.wallace@gmail.com>
2019-08-07 19:43:23 -0600

    first commit

---
+++ server/services/GodsService.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose"
+const Schema = mongoose.Schema
+const ObjectId = Schema.Types.ObjectId
+
+const _model = new Schema({
+    Name: { type: String, required: true },
+    description: { type: String, required: true },
+    godsStory: { type: String, required: true },
+    godOfWhat: { type: String, required: true },
+    siblings: { type: String },
+    children: { type: String },
+    aesir: { type: Boolean, required: true },
+    vanir: { type: Boolean, required: true }
+})
+
+export default class GodsService {
+    get repository() {
+        return mongoose.model('Gods', _model)
+    }
+}
