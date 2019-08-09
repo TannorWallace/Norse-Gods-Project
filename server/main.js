ac3b4aa6fda40cfe4c4c844b80449e4db77e064b
TannorWallace <tannor.wallace@gmail.com>
2019-08-08 20:16:31 -0600

    ok maybe got it the server issue resolved?

--- server/main.js
+++ server/main.js
@@ -42,10 +42,10 @@ server.use('/account', new AuthController().router)
 //NOTE next we want to register all our routes(doorways that can be accessed in our app)
 
 //NOTE we have to import access to our controllers
-import ValuesController from './controllers/ValuesController'
+import GodsController from './controllers/GodsController'
 
 //NOTE remember the forward slash at the start of your path!
-server.use('/api/values', new ValuesController().router)
+server.use('/api/NorseGods', new GodsController().router)
 
 
 
